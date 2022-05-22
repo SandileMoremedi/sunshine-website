71d0eaad8ddd10e1aef4c3c98a552ea9f072996a
Jus' A Mere Dev <sandilemoremedi070501@gmail.com>
2022-05-22 16:03:59 +0200

    Made the base boilerplate for the auth

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import { useState } from "react";
+import { useUser } from "@auth0/nextjs-auth0";
 
 const Header = () => {
   const [navbar, setNavbar] = useState(false);
+  const { user, error, isLoading } = useUser();
   return (
     <header className="header">
       <nav className="nav">
@@ -29,7 +31,11 @@ const Header = () => {
           </li>
         </ul>
         <div className="log">
-          <Link href="/api/auth/login">Login</Link>
+          {user ? (
+            <Link href="/api/auth/login">Logout</Link>
+          ) : (
+            <Link href="/api/auth/login">Login</Link>
+          )}
         </div>
       </nav>
     </header>
